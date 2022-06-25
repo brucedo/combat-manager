a36643dc1944b1a48535c67ce8d26cd7d2d3ca71
Doug Bruce <queazan@gmail.com>
2022-06-25 14:20:58 -0500

    - Quick sidestep.  Poem is simple and effective as a web API but seems to take statelessness as a given.  However, I need to interact with persistent and stateful game data.    Now, I _can_ do this with the make() function - make() gives me the freedom to hand in a closure to be turned into an Endpoint, but by doing so I lose the ability to let Poem    filter requests by method, path etc. and to have Poem handle uri path segment conversions for me.  That basically leaves me no better off than using raw Hyper.  - So, we're going to hold on here for a bit and experiment in another branch with doing all of this in Rocket instead.  Rocket supports some innate stateful behaviour.    - The driving force for me to try Poem was that it supports WebSockets, which I felt would provide me with later options for more dynamic client<->server interactions.  However, Rocket      _does_ support Server-Side Events - so I can possibly get the best of both worlds.

--- src/dispatcher/mod.rs
+++ src/dispatcher/mod.rs
@@ -1,6 +1,7 @@
 use log::debug;
-use poem::{Route, get, handler, listener::TcpListener, Server, Request};
+use poem::{Route, get, handler, listener::TcpListener, Server, Request, web::Path};
 use tokio::sync::mpsc::Sender;
+use uuid::Uuid;
 
 use crate::MainMessages;
 
@@ -13,7 +14,9 @@ pub async fn launch_server(main_sender: Sender<MainMessages>)
         let moved = main_sender.clone();
         async move {api_handler(moved, req).await;}
     });
-    let routes = Route::new().at("/api", get(made_api_handler));
+    let routes = Route::new().at("/api", get(made_api_handler))
+        .at("/api/:game_id", |demo: Path<String>| {let moved = main_sender.clone(); async move {new_game(moved, demo).await}})
+    ;
     Server::new(TcpListener::bind("localhost:8080")).run(routes).await;
 }
 
@@ -26,5 +29,10 @@ pub fn bootstrap() -> &'static str
 
 pub async fn api_handler(sender: Sender<MainMessages>, req: Request)
 {
+    // req.
+}
 
+pub async fn new_game(sender: Sender<MainMessages>, Path(demo): Path<String>) -> &'static str
+{
+    return "what shite is this";
 }
