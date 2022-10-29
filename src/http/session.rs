691556444c06e3d11dae12714a5c8f70db8e30f5
Doug Bruce <queazan@gmail.com>
2022-10-29 17:22:49 -0500

    Progress...

--- src/http/session.rs
+++ src/http/session.rs
@@ -1,9 +1,6 @@
-use std::{sync::Arc, collections::HashMap};
-use core::pin::Pin;
-use core::future::Future;
-use core::marker::Send;
+use std::{collections::HashMap, sync::Arc};
 use parking_lot::{RwLock, Mutex};
-use rocket::{Request, request::{FromRequest, Outcome}};
+use rocket::{Request, request::{FromRequest, Outcome, self}};
 use uuid::Uuid;
 
 use super::errors::Error;
@@ -15,7 +12,20 @@ pub struct SessionData
 
 pub struct Session
 {
-    session_data: Mutex<SessionData>
+    session_data: Arc<Mutex<SessionData>>
+}
+
+impl Session
+{
+    pub fn new() -> Session
+    {
+        Session { session_data: Arc::new(Mutex::new(SessionData {gm_of_games: Vec::new()})) }
+    }
+
+    pub fn clone(&self) -> Session
+    {
+        Session { session_data: self.session_data.clone()}
+    }
 }
 
 pub struct SessionMap
@@ -23,26 +33,47 @@ pub struct SessionMap
     sessions: RwLock<HashMap<Uuid, Session>>
 }
 
-pub fn new() -> SessionMap
-{
-    SessionMap { sessions: RwLock::new(HashMap::new()) }
-}
+
 
 impl SessionMap
 {
-    pub fn find_session(id: Uuid) -> Option<&Session>
+    pub fn new() -> SessionMap
+    {
+        SessionMap { sessions: RwLock::new(HashMap::new()) }
+    }
+
+    pub fn find_session(&self, id: Uuid) -> Option<Session>
     {
 
+        if let Some(temp) = self.sessions.read().get(&id)
+        {
+            return Some(temp.clone());
+        }
+        else
+        {
+            None
+        }
     }
+
+    pub fn add_session(&mut self, id: Uuid, session: Session)
+    {
+        self.sessions.write().insert(id, session);
+    }
+}
+
+#[derive(Debug)]
+pub struct NoSession
+{
+
 }
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for Session
 {
-    type Error = Error;
+    type Error = NoSession;
 
-    async fn from_request(request: & 'r Request<'_>) ->  Outcome<Self,Self::Error>
-    where 'r: 'async_trait, 'life0: 'async_trait,Self: 'async_trait 
+    async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error>
+    // where 'r: 'async_trait, 'life0: 'async_trait,Self: 'async_trait 
     {
         match request.cookies().get("shadowrun_combat_session")
         {
@@ -52,23 +83,19 @@ impl<'r> FromRequest<'r> for Session
                 match Uuid::parse_str(session_cookie.value())
                 {
                     Ok(temp) => { session_id = temp; },
-                    Err(_) =>  {todo!()}
+                    Err(_) =>  {session_id = Uuid::new_v4()}
                 }
 
-                let mut map = request.rocket().state::<SessionMap>();
-                
+                let map = request.rocket().state::<SessionMap>().unwrap_or_else(|| panic!());
 
-                match map.sessions.entry(session_id)
+                match map.find_session(session_id)
                 {
-                    std::collections::hash_map::Entry::Occupied(_) => 
-                    {
-
-                    },
-                    std::collections::hash_map::Entry::Vacant(_) => todo!(),
+                    Some(session) => {return Outcome::Success(session);},
+                    None => {return Outcome::Forward(())},
                 }
             },
-            None => todo!(),
+            None => return Outcome::Forward(()),
         };
-        Outcome::Success(Session {gm_of_games: Vec::new()})
+        // Outcome::Success(Session::new())
     }
 }
