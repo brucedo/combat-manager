eb7af81d73943d61bbfc6720e53a542d96bf0a70
Doug Bruce <queazan@gmail.com>
2022-11-19 14:15:02 -0600

    - updated the build.sh script.  It's still pretty crap but it'll let me spin out a linux native and windows cross compile, and package each into separate compressed archives for distribution.    So, it's good enough for now.

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ CFG_NAME="Rocket.toml"
 DIST=dist
 STATIC="resources/static"
 TEMPLATES="resources/templates"
+BUILD_SUFFIX=""
 DIST_STATIC="${DIST}"/${STATIC}
 DIST_TEMPLATES="${DIST}"/${TEMPLATES}
 
@@ -19,26 +20,57 @@ while true; do
     esac
 done
 
-
 if [ $CLEAN == true ] ;
 then
+    cargo clean
     [ -d "${DIST}/" ] && rm -R "${DIST}/"
     exit 0;
 fi
 
+
 if [ $RELEASE == true ] ; 
 then
     BIN_SRC="target/release"
+    BUILD_SUFFIX="$BUILD_SUFFIX--release"
 else
     BIN_SRC="target/debug"
 fi
 
-BIN_NAME="${BIN_SRC}/${BIN_NAME}"
+# Build Linux release...
+cargo build "$BUILD_SUFFIX"
+
+SRC_BIN_NAME="${BIN_SRC}/${BIN_NAME}"
 
 [ ! -d "${DIST_STATIC}/" ] && mkdir -p "${DIST_STATIC}"
 [ ! -d "${DIST_TEMPLATES}/" ] && mkdir -p "${DIST_TEMPLATES}"
 
-cp "${BIN_NAME}" "${DIST}/"
+
+cp "${SRC_BIN_NAME}" "${DIST}/"
 cp "${CFG_NAME}" "${DIST}/"
-cp -R "${STATIC}" "${DIST_STATIC}" 
-cp -R "${TEMPLATES}" "${DIST_TEMPLATES}"
+cp -R ${STATIC}/* "${DIST_STATIC}/" 
+cp -R ${TEMPLATES}/* "${DIST_TEMPLATES}/"
+
+# package Linux release
+tar -cvJf shadowrun-linux-x86_64.tar.xz ${DIST}/
+
+# clear Linux bin in preparation for windows cross build
+rm "${DIST}"/"${BIN_NAME}"
+
+# build Windows release.  This should all be a bit less copy pasta from above...
+cargo build --target x86_64-pc-windows-gnu "$BUILD_SUFFIX"
+
+if [ $RELEASE == true ] ; 
+then
+    BIN_SRC="target/x86_64-pc-windows-gnu/release"
+    BUILD_SUFFIX="$BUILD_SUFFIX--release"
+else
+    BIN_SRC="target/x86_64-pc-windows-gnu/debug"
+fi
+
+SRC_BIN_NAME="${BIN_SRC}/${BIN_NAME}.exe"
+cp "${SRC_BIN_NAME}" "${DIST}/"
+
+echo $SRC_BIN_NAME and $BIN_SRC
+
+# package Windows release
+zip -9r shadowrun-windows-x86_64.zip ${DIST}/
