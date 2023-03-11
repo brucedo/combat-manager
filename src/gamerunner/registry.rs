5344d3350c4a1a048219f8d0a9e2d1ef4a256da4
Doug Bruce <queazan@gmail.com>
2023-03-11 16:49:32 -0600

    - Added more functionality to the registry struct.

--- src/gamerunner/registry.rs
+++ src/gamerunner/registry.rs
@@ -1,6 +1,5 @@
 use std::collections::{HashMap, HashSet};
-
-use rocket::serde::json::serde_json::map::Entry;
+use std::collections::hash_map::Entry as MapEntry;
 use tokio::sync::mpsc::Sender;
 use uuid::Uuid;
 
@@ -58,8 +57,8 @@ impl <'a> GameRegistry
     {
         match self.players.entry(player_id)
         {
-            std::collections::hash_map::Entry::Occupied(_) => Err(()),
-            std::collections::hash_map::Entry::Vacant(vacant) => 
+            MapEntry::Occupied(_) => Err(()),
+            MapEntry::Vacant(vacant) => 
             {
                 vacant.insert(PlayerDirectoryEntry { player_id, player_games: HashSet::new(), player_characters: HashSet::new(), player_sender: player_comm_channel });
                 Ok(())
@@ -89,14 +88,94 @@ impl <'a> GameRegistry
     {
         match self.players.entry(player_id)
         {
-            std::collections::hash_map::Entry::Occupied(player_dir) => 
+            MapEntry::Occupied(player_dir) => 
             {
                 Some(player_dir.get().player_sender.clone())
             },
-            std::collections::hash_map::Entry::Vacant(_) => 
+            MapEntry::Vacant(_) => 
             {
                 None
             },
+        }
+    }
+
+    pub fn game_has_player(&self, game_id: Uuid, player_id: Uuid) -> bool
+    {
+        self.games.contains_key(&game_id) && self.games.get(&game_id).unwrap().players.contains(&player_id)
+    }
+
+    pub fn player_in_game(&self, player_id: Uuid, game_id: Uuid) -> bool
+    {
+        self.players.contains_key(&player_id) && self.players.get(&player_id).unwrap().player_games.contains(&game_id)
+    }
+
+    pub fn games_by_player(&self, player_id: Uuid) -> Option<&HashSet<Uuid>>
+    {
+        if self.players.contains_key(&player_id)
+        {
+            Some(&self.players.get(&player_id).unwrap().player_games)
+        }
+        else
+        {
+            None
+        }
+    }
+
+    pub fn players_by_game(&mut self, game_id: Uuid) -> Option<&HashSet<Uuid>>
+    {
+        if self.games.contains_key(&game_id)
+        {
+            Some(&self.games.get(&game_id).unwrap().players)
+        }
+        else
+        {
+            None
+        }
+    }
+
+    pub fn leave_game(&mut self, player_id: Uuid, game_id: Uuid) -> Result<(), ()>
+    {
+        match (self.games.entry(game_id), self.players.entry(player_id))
+        {
+            (MapEntry::Occupied(mut game_entry), MapEntry::Occupied(mut player_entry)) =>
+            {
+                let removed_player = game_entry.get_mut().players.remove(&player_id);
+                let removed_game = player_entry.get_mut().player_games.remove(&game_id);
+                if !removed_player || !removed_game
+                {
+                    return Err(());
+                }
+                else
+                {
+                    return Ok(());
+                }
+            },
+            _ => {Err(())}
+        }
+    }
+
+    pub fn delete_game(&mut self, game_id: Uuid) -> Result<(), ()>
+    {
+        if let Some(game) = self.games.remove(&game_id)
+        {
+            let mut players = game.players;
+
+            for player in players.drain()
+            {
+                match self.players.entry(player)
+                {
+                    MapEntry::Occupied(mut player_entry) => {
+                        player_entry.get_mut().player_games.remove(&game_id);
+                    },
+                    MapEntry::Vacant(_) => {}
+                }
+            }
+
+            Ok(())
+        }
+        else
+        {
+            return Err(())
         }
     }
 }
@@ -104,6 +183,8 @@ impl <'a> GameRegistry
 #[cfg(test)]
 pub mod tests
 {
+    use std::collections::HashSet;
+
     use tokio::sync::mpsc::channel;
     use uuid::Uuid;
 
@@ -164,7 +245,7 @@ pub mod tests
     }
 
     #[test]
-    pub fn players_who_is_registered_may_can_join_a_game()
+    pub fn players_who_is_registered_may_join_a_game()
     {
         let mut registry = GameRegistry::new();
 
@@ -211,4 +292,184 @@ pub mod tests
 
         assert!(receiver.recv().await.is_some());
     }
+
+    #[test]
+    pub fn a_player_may_enumerate_the_games_they_are_in()
+    {
+        init();
+        let mut registry = GameRegistry::new();
+        let game_1 = Uuid::new_v4();
+        let game_2 = Uuid::new_v4();
+        let game_3 = Uuid::new_v4();
+
+        let (mut sender, _) = channel(32);
+        let player_1 = Uuid::new_v4();
+
+
+        registry.new_game(game_1, Game::new());
+        registry.new_game(game_2, Game::new());
+        registry.new_game(game_3, Game::new());
+        registry.register_player(player_1, sender);
+
+        registry.join_game(player_1, game_1);
+        registry.join_game(player_1, game_2);
+        registry.join_game(player_1, game_3);
+
+        let games: &HashSet<Uuid> = registry.games_by_player(player_1).unwrap();
+
+        assert_eq!(3, games.len());
+        assert!(games.contains(&game_1));
+        assert!(games.contains(&game_2));
+        assert!(games.contains(&game_3));
+    }
+
+    #[test]
+    pub fn if_the_argument_to_games_by_player_does_not_resolve_to_a_registered_player_none_is_returned()
+    {
+        init();
+        let mut registry = GameRegistry::new();
+        let game_1 = Uuid::new_v4();
+
+        let (mut sender, _) = channel(32);
+        let player_1 = Uuid::new_v4();
+
+        registry.new_game(game_1, Game::new());
+        registry.register_player(player_1, sender);
+
+        assert!(registry.games_by_player(Uuid::new_v4()).is_none());
+
+    }
+
+    #[test]
+    pub fn a_game_may_enumerate_the_players_who_have_joined_it()
+    {
+        let mut registry = GameRegistry::new();
+        let game_1 = Uuid::new_v4();
+
+        let player_1 = Uuid::new_v4();
+        let (sender_1, _) = channel(32);
+        let player_2 = Uuid::new_v4();
+        let (sender_2, _) = channel(32);
+        let player_3 = Uuid::new_v4();
+        let (sender_3, _) = channel(32);
+
+        registry.register_player(player_1, sender_1);
+        registry.register_player(player_2, sender_2);
+        registry.register_player(player_3, sender_3);
+
+        registry.new_game(game_1, Game::new());
+
+        registry.join_game(player_1, game_1);
+        registry.join_game(player_2, game_1);
+        registry.join_game(player_3, game_1);
+
+        let players: &HashSet<Uuid> = registry.players_by_game(game_1).unwrap();
+
+        assert_eq!(3, players.len());
+        assert!(players.contains(&player_1));
+        assert!(players.contains(&player_2));
+        assert!(players.contains(&player_3));
+    }
+
+    #[test]
+    pub fn if_the_argument_to_players_by_game_does_not_resolve_to_a_real_game_then_none_is_returned()
+    {
+        init();
+        let mut registry = GameRegistry::new();
+        
+        let game_1 = Uuid::new_v4();
+
+        let player_1 = Uuid::new_v4();
+        let (sender, _) = channel(32);
+
+        registry.new_game(game_1, Game::new());
+        registry.register_player(player_1, sender);
+
+        registry.join_game(player_1, game_1);
+
+        assert!(registry.players_by_game(Uuid::new_v4()).is_none());
+    }
+
+    #[test]
+    pub fn if_a_player_leaves_a_game_both_the_player_and_the_game_records_update_to_remove_the_player()
+    {
+        init();
+
+        let mut registry = GameRegistry::new();
+        let player_1 = Uuid::new_v4();
+        let player_2 = Uuid::new_v4();
+        let (mut sender, _) = channel(32);
+        let game_id = Uuid::new_v4();
+        let game = Game::new();
+
+        registry.new_game(game_id, game);
+        registry.register_player(player_1, sender);
+        (sender, _) = channel(32);
+        registry.register_player(player_2, sender);
+
+        registry.join_game(player_1, game_id);
+        registry.join_game(player_2, game_id);
+
+        assert!(registry.leave_game(player_1, game_id).is_ok());
+        assert!(!registry.game_has_player(game_id, player_1));
+        assert!(!registry.player_in_game(player_1, game_id));
+    }
+
+    #[test]
+    pub fn if_a_player_leaves_a_game_they_are_not_a_member_of_leave_game_returns_err()
+    {
+        init();
+
+        let mut registry = GameRegistry::new();
+        let player_1 = Uuid::new_v4();
+        let game_id = Uuid::new_v4();
+        let game = Game::new();
+
+        registry.new_game(game_id, game);
+
+        assert!(registry.leave_game(player_1, game_id).is_err());
+    }
+
+    #[test]
+    pub fn when_a_game_is_deleted_all_players_are_updated_to_remove_that_game_from_their_lists()
+    {
+        init();
+
+        let mut registry = GameRegistry::new();
+        let player_1 = Uuid::new_v4();
+        let (sender_1, _) = channel(32);
+        let player_2 = Uuid::new_v4();
+        let (sender_2, _) = channel(32);
+        let player_3 = Uuid::new_v4();
+        let (sender_3, _) = channel(32);
+        
+        let game_id = Uuid::new_v4();
+
+        registry.new_game(game_id, Game::new());
+        registry.register_player(player_1, sender_1);
+        registry.register_player(player_2, sender_2);
+        registry.register_player(player_3, sender_3);
+
+        registry.join_game(player_1, game_id);
+        registry.join_game(player_2, game_id);
+        registry.join_game(player_3, game_id);
+
+        registry.delete_game(game_id);
+
+        assert!(registry.players_by_game(game_id).is_none());
+        assert!(!registry.player_in_game(player_1, game_id));
+        assert!(!registry.player_in_game(player_2, game_id));
+        assert!(!registry.player_in_game(player_3, game_id));
+        
+    }
+
+    pub fn when_delete_game_is_passed_an_id_that_does_not_represent_a_real_game_err_is_returned()
+    {
+        init();
+
+        let mut registry = GameRegistry::new();
+
+        assert!(registry.delete_game(Uuid::new_v4()).is_err());
+    }
+
 }
